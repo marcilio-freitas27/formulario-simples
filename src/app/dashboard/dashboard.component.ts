952c150a7e9d5d4ab4ddb0eb2a1475f45f7fea3f
Marcílio Freitas <36551921+marcilio-freitas27@users.noreply.github.com>
2024-03-16 12:59:13 +0000

    Inserção do gráfico com chartsjs.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Chart, registerables} from 'chart.js';
+import { CadastroService } from '../service/cadastro.service';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -8,18 +11,27 @@ import { Chart } from 'chart.js';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private cadastroService: CadastroService) {
+
+   }
 
   ngOnInit(): void {
+    let usuarios = this.cadastroService.listarUsuario();
+    let usuarioNome: any[];
+    usuarioNome = [];
+    for (let index = 0; index < usuarios.length; index++) {
+      usuarioNome.push(usuarios[index].nome)
+    }
     const ctx: any = document.getElementById('myChart');
 
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: usuarioNome,
         datasets: [{
           label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          data: [12, 19, 3, 5, 2, 3, 9,7,1,4],
           borderWidth: 1
         }]
       },
